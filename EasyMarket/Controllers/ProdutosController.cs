225cde1f97c0d17769a9e979235e284610688393
RaFael Felipe Leite Duarte <rafinha.lduarte@hotmail.com>
2019-09-22 18:11:51 -0300

    att

---
+++ EasyMarket/Controllers/ProdutosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyMarket.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        // GET: Produtos
+        public ActionResult ArrozGraos()
+        {
+            return View();
+        }
+
+        public ActionResult Single()
+        {
+            return View();
+        }
+    }
+}
